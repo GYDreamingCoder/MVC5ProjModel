ff535bedcb35896d87fe8b5b5ee119ef14cdb283
扬 高 <yang.gao@flowportal.com>
2021-02-28 19:27:01 +0800

    2021/2/28 19:26

--- MVC5ProjModel/Controllers/HomeController.cs
+++ MVC5ProjModel/Controllers/HomeController.cs
@@ -3,16 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVC5ProjModel.Models;
 
 namespace MVC5ProjModel.Controllers
 {
     public class HomeController : Controller
     {
+        MusicStoreEntities storeDB = new MusicStoreEntities();
+
         [HandleError(ExceptionType = typeof(Exception), View = "MyError")]
         public ActionResult Index()
         {
             //throw new Exception("this is a Error Page");
-            return View();
+            var albmums = GetTopSellingAlbums(5);
+            return View(albmums);
         }
 
         public ActionResult About()
@@ -27,6 +31,16 @@ namespace MVC5ProjModel.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        /// <summary>
+        /// 获取首页在售专辑
+        /// </summary>
+        /// <param name="count"></param>
+        /// <returns></returns>
+        private List<Album> GetTopSellingAlbums(int count) {
+            //根据订单数量来排序,返回订单数量最多的n个专辑
+            return storeDB.Albums.OrderByDescending(a => a.OrderDetails.Count()).Take(count).ToList();
         }
     }
 }
--- MVC5ProjModel/Models/AccountModels.cs
+++ MVC5ProjModel/Models/AccountModels.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.ComponentModel.DataAnnotations;//提供用于为 ASP.NET MVC 和 ASP.NET 数据控件定义元数据的特性类。
+using System.Web.Security;
+
 
 namespace MVC5ProjModel.Models
 {
@@ -13,5 +15,55 @@ namespace MVC5ProjModel.Models
         [DataType(DataType.Password)]
         [Display(Name ="Current password")]
         public string OldPassword { get; set; }
+
+        [Required]
+        [StringLength(100,ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name ="New Password")]
+        public string NewPassword { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name ="Confirm New Password")]
+        [System.ComponentModel.DataAnnotations.Compare("NewPassword",ErrorMessage ="not Same!!!!")]
+        public string ConfirmePassword { get; set; }
+    }
+
+    public class logOnModel {
+        [Key]
+
+        [Required]
+        [Display(Name ="User Name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name ="PassWord")]
+        public string Password { get; set; }
+
+        [Display(Name ="Remember Me?")]
+        public bool RememberMe { get; set; }
+    }
+
+    public class RegisterModel {
+        [Required]
+        [Display(Name ="User Name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [Display(Name ="Password")]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name ="Email")]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm New Password")]
+        [System.ComponentModel.DataAnnotations.Compare("NewPassword", ErrorMessage = "not Same!!!!")]
+        public string ConfirmPassword { get; set; }
     }
 }
